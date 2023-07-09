4f41aa2f3a769c19de354912c4c84685cd4e7b8d
Shafiq Salehi <rasashafiq@gmail.com>
2023-07-09 18:57:25 +0200

    typed the entire app

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 
+//@ts-ignore
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
