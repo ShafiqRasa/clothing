3ba6c779e46ebe2128851cab39af615be1c55770
Shafiq Salehi <rasashafiq@gmail.com>
2023-07-11 14:21:18 +0200

    added redux-persist only

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,22 @@
 import { createStore, compose, applyMiddleware } from "redux";
 // import logger from "redux-logger";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { loggerMiddleware } from "./middlerware/logger";
 import { rootReducer } from "./root-reducer";
 import thunk from "redux-thunk";
 
+const persistConfiguation = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
+const persistedReducer = persistReducer(persistConfiguation, rootReducer);
+
 const middlerwares = [loggerMiddleware, thunk];
 const composeEnhancer = compose(applyMiddleware(...middlerwares));
 
-export const store = createStore(rootReducer, undefined, composeEnhancer);
+export const store = createStore(persistedReducer, undefined, composeEnhancer);
+
+export const persistor = persistStore(store);
